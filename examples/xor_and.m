3b00a37663caaf4a046d3667a1f61ce0a0dbd121
Augusto Damasceno <augustomatheuss@yahoo.com.br>
2015-08-28 18:26:20 -0300

    Create xor_and.m

---
+++ examples/xor_and.m
@@ -0,0 +1,25 @@
+% Configuration
+neurons = [5 2];
+a = 0.0001;
+bias = [-1 -1];
+x = [0 0;0 1;1 0; 1 1];
+yref = [0 0;1 0;1 0;0 1];
+lr = 0.7;
+error = 10^-20;
+maxIt = 100000;
+
+% Processing
+[W1,W2,E] = trainingMLP2(neurons,a,bias,x,yref,lr,error,maxIt);
+
+% Display Infos
+disp('Error');
+disp(E);
+disp('XOR AND')
+disp('Input [0 0]');
+outMLP2(bias,[0 0],W1,W2)
+disp('Input [0 1]');
+outMLP2(bias,[0 1],W1,W2)
+disp('Input [1 0]');
+outMLP2(bias,[1 0],W1,W2)
+disp('Input [1 1]');
+outMLP2(bias,[1 1],W1,W2)

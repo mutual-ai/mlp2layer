8e186e57417cc3807fa5022f5897277cf08476cf
Augusto Damasceno <augustomatheuss@yahoo.com.br>
2015-08-31 21:45:54 -0300

    Create xor.m

---
+++ examples/xor.m
@@ -0,0 +1,28 @@
+% Configuration
+neurons = [4 1];
+a = 0.001;
+bias = [-1 -1];
+x = [0 0;0 1;1 0; 1 1];
+yref = [0 ;1 ;1 ;0];
+lr = 0.7;
+error = 10^-20;
+maxIt = 1e6;
+
+% Processing
+[W1,W2,E] = trainingMLP2(neurons,a,bias,x,yref,lr,error,maxIt);
+
+% Display Infos
+disp('Error');
+disp(E);
+disp('XOR')
+disp('Input [0 0]');
+outMLP2(bias,[0 0],W1,W2)
+disp('Input [0 1]');
+outMLP2(bias,[0 1],W1,W2)
+disp('Input [1 0]');
+outMLP2(bias,[1 0],W1,W2)
+disp('Input [1 1]');
+outMLP2(bias,[1 1],W1,W2)
+
+% Plot MSE
+semilogx(mse_hist)
